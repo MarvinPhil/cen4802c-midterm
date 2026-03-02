bf74f5c1500934f39f0875237be05ca905c1b7f3
MarvinPhil <160289274+MarvinPhil@users.noreply.github.com>
2026-03-01 21:57:09 -0500

    No longer takes user input(simplify automation process)

--- src/main/java/org/example/AverageCalc.java
+++ src/main/java/org/example/AverageCalc.java
@@ -1,29 +1,35 @@
 package org.example;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class AverageCalc {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+        //Scanner scn = new Scanner(System.in);
 
-        System.out.println("How many numbers do you want to add?:");
-        int n = scn.nextInt();
-        scn.nextLine();
+        System.out.println("How many numbers do you want to add?: 3");
+        //int n = scn.nextInt();
+        //scn.nextLine();
+        int n = 3;
+        double[] numbers = {10.0, 20.0, 30.0};
 
-        double result = averager(n, scn);
+        double result = averager(n, numbers);
 
         //testing pipeline
         System.out.printf("The average is %.2f", result);
     }
 
-    public static double averager(double n, Scanner scn){
+    public static double averager(double n, double[] numbers){
         double sum = 0;
+        if (numbers.length == 0) return 0;
+        for (double num : numbers) {
+            sum += num;
+        }
 
-        for (int i = 1; i <= n; i++) {
+        /*for (int i = 1; i <= n; i++) {
             System.out.println(i+".Enter a number:");
             sum += scn.nextDouble();
             scn.nextLine();
-        }
+        }*/
 
         return sum / n;
     }
--- src/test/java/org/example/AvgCalcTest.java
+++ src/test/java/org/example/AvgCalcTest.java
@@ -3,13 +3,13 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AvgCalcTest {
 
-
+/*
     @Test
     @DisplayName("Normal test")
     public void test() {
@@ -32,5 +32,37 @@ public class AvgCalcTest {
         String simulatedInput = "10\n-10\n";
         Scanner testScanner = new Scanner(simulatedInput);
         assertEquals(0.00, AverageCalc.averager(2, testScanner), 0);
+    }
+
+ */
+
+    @Test
+    @DisplayName("Normal test")
+    public void test1(){
+        int numOfNum = 5;
+        double[] input = {23.0, 23.0, 23.0, 23.0, 23.0};
+        double expected = 23.0;
+
+        assertEquals(expected, AverageCalc.averager(numOfNum, input), 0.01);
+    }
+
+    @Test
+    @DisplayName("Negative test")
+    public void test2(){
+        int numOfNum = 3;
+        double[] input = {-20.0, -20.0, -20.0};
+        double expected = -20.0;
+
+        assertEquals(expected, AverageCalc.averager(numOfNum, input), 0.01);
+    }
+
+    @Test
+    @DisplayName("Mixed test")
+    public void test3(){
+        int numOfNum = 2;
+        double[] input = {10.0, -10.0};
+        double expected = 0.0;
+
+        assertEquals(expected, AverageCalc.averager(numOfNum, input), 0.01);
     }
 }
